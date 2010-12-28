fde593958a91d80d6571b120d1e662947d0a879f
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-12-27 22:41:28 -0700

    Added a basic search results page

--- app/gitdex.rb
+++ app/gitdex.rb
@@ -1,4 +1,5 @@
-require "repos.rb"
+require "repos"
+require "app/search"
 
 set :views, File.dirname(__FILE__) + '/views'
 set :public, File.dirname(__FILE__) + '/public'
@@ -9,4 +10,12 @@ get '/' do
   available_repos.sort.each_slice(5) { |repo_list| repos << repo_list }
   
   haml :home, :locals => { :repos => repos }
+end
+
+
+get '/search' do
+  # perform the search
+  results = Search.search params[:query]
+  
+  haml :search, :locals => { :results => results }
 end
