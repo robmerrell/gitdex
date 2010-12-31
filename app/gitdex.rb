2366a843c33fd96ed2963d5337362e1addaae162
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-12-31 09:16:54 -0700

    Added the recent searches using redis

--- app/gitdex.rb
+++ app/gitdex.rb
@@ -1,19 +1,40 @@
 require "app/repos"
 require "app/search"
+require "uri"
+require "cgi" # used for url encoding
 
 set :views, File.dirname(__FILE__) + '/views'
 set :public, File.dirname(__FILE__) + '/public'
 
-get '/' do
+configure do
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+
+get '/' do  
   # split the repos up into 4 groups for the listing
   repos = []
   available_repos.sort.each_slice(5) { |repo_list| repos << repo_list }
+
+  # pull the most recent searches from redis
+  searches = REDIS.lrange "searches", 0, 10
   
-  haml :home, :locals => { :repos => repos }
+  haml :home, :locals => { :repos => repos, :searches => searches }
 end
 
 
 get '/search' do
+  # remove the oldest search, and push this search onto the list
+  if params[:page].nil? || params[:page] == 0
+    # get the last element pushed onto the list without removing it
+    index = REDIS.llen("searches") - 1
+    last = REDIS.lrange("searches", index, index).first
+    
+    REDIS.lpop("searches") if REDIS.llen("searches") >= 10
+    REDIS.rpush("searches", params[:query]) if params[:query] != last
+  end
+  
   page = params[:page].to_i || 0
   per_page = 10
   
